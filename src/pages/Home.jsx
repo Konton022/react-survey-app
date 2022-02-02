d266263d4eaa154b94315f3fc3ba62f7081c423e
KonstantinSav <koston022@mail.ru>
2022-02-02 17:22:19 +0500

    added tailwingcss lib

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,23 @@
 
 import React, {useEffect, useState} from 'react';
 import db, { getSurveys } from '../firebase'
-
+import Input from '../components/Input';
 
 const Home = () => {
 
     const [value, setValue] = useState([]);
    
-    useEffect(async ()=> {
-       const surv = await getSurveys(db)
-       setValue(surv)
+    useEffect(()=> {
+        getSurveys(db).then((data => setValue(data)))
+       
     }, [])
 
     return <div>
+
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Input /> 
             {!value.length? <div>loading...</div>: value.length}
             
         </div>
